5782fa3a9969c2bad5e928fe2eff30089397f742
JuanmaLopezz <juanmalopez9@gmail.com>
2020-03-02 12:22:46 +0100

    Update StockInformationProcessor.java

--- vijumi/src/main/java/com/oesia/formacion/practica/architecture/communications/processors/StockInformationProcessor.java
+++ vijumi/src/main/java/com/oesia/formacion/practica/architecture/communications/processors/StockInformationProcessor.java
@@ -2,38 +2,45 @@ package com.oesia.formacion.practica.architecture.communications.processors;
 
 import java.util.List;
 
+import com.oesia.formacion.practica.architecture.communications.MessageManager;
 import com.oesia.formacion.practica.architecture.communications.messages.stock.StockMessageInformation;
 import com.oesia.formacion.practica.architecture.communications.messages.stock.StockMessageInformation.Builder;
+import com.oesia.formacion.practica.architecture.domain.managers.article.ArticleManager;
+import com.oesia.formacion.practica.architecture.domain.model.Article;
 import com.oesia.formacion.practica.architecture.domain.model.Message;
+import com.oesia.formacion.practica.context.ContextFactory;
 
 public class StockInformationProcessor implements Processor {
 
 	private static final int STOCK_ARTICLE_ID_POSITION = 0;
-	private static final int STOCK_DESCRIPTION_POSITION = 1;
-	private static final int STOCK_COLOUR_ID_POSITION = 2;
-	private static final int STOCK_SIZE_ID_POSITION = 3;
-	private static final int STOCK_NUM_UNIT_POSITION = 6;
 
 	@Override
+
 	public void process(Message message) {
 
 		for (List<String> row : message.getRecords()) {
 
 			int idArticle = Integer.parseInt(row.get(STOCK_ARTICLE_ID_POSITION));
-			String descriptionArticle = row.get(STOCK_DESCRIPTION_POSITION);
-			int idColor = Integer.parseInt(row.get(STOCK_COLOUR_ID_POSITION));
-			int idTalla = Integer.parseInt(row.get(STOCK_SIZE_ID_POSITION));
-			int numUds = Integer.parseInt(row.get(STOCK_NUM_UNIT_POSITION));
 
-			StockMessageInformation.Builder stockBuilder = new Builder();
-			stockBuilder.idArticle(idArticle);
-			stockBuilder.descriptionArticle(descriptionArticle);
-			stockBuilder.idColor(idColor);
-			stockBuilder.numUds(numUds);
-			stockBuilder.idTalla(idTalla);
+			ArticleManager articleManager = ContextFactory.getContext().get(ArticleManager.class);
+			MessageManager messageManager = ContextFactory.getContext().get(MessageManager.class);
+			
+			Article articleDataBase = articleManager.findById(idArticle);
+			
+			if (articleDataBase != null) {
+				StockMessageInformation.Builder stockBuilder = new Builder();
+				stockBuilder.idArticle(idArticle);
+				stockBuilder.descriptionArticle(articleDataBase.getDescription());
+				stockBuilder.idColor(articleDataBase.getColour().getId());
+				stockBuilder.idTalla(articleDataBase.getSize().getId());
+				stockBuilder.numUds(articleDataBase.getNumUnit());
+				
+				messageManager.send(String.format("STOCK|%s|%s|%s|%s|%s", idArticle, articleDataBase.getDescription(), articleDataBase.getColour(), articleDataBase.getSize(), articleDataBase.getNumUnit()));
 
-			StockMessageInformation stockMessageInformation = stockBuilder.build();
-
+				StockMessageInformation stockMessageInformation = stockBuilder.build();
+			} else {
+				messageManager.send("KO");
+			}
 		}
 	}
 }
